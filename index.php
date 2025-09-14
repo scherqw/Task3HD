4948df22d7378020a65765535eb74699acfd6554
Chhun Hong Lim <honglimchhun@gmail.com>
2025-09-14 14:29:21 +1000

    created a random number generator.

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
-﻿<?php
+<?php
+$randNum = 0;
 
-echo 'Hello World!';
+if (isset($_POST["random"])) {
+    $randNum = rand(1, 10);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Random Number Generator</title>
+</head>
+<body>
+
+    <div>
+        <h1>Random Number Generator</h1>
+        <p>Generate a random number from 1 to 10.</p>
+
+        <form action="index.php" method="post">
+            <div>
+                <h2><?php echo "Random Number: " . $randNum?></h2>
+            </div>
+            <button type="submit" name="random">Generate Random Number</button>
+        </form>
+    </div>
+
+</body>
+</html>
+
